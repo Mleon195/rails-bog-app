3588a93c2aa9e5cf2d021dd69f219dd1a2bf37ff
Mleon195 <mleon@mail.ccsf.edu>
2016-04-29 17:04:54 -0700

    new create and show working

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -2,5 +2,10 @@
 # config/routes.rb
 #
 Rails.application.routes.draw do
+  root to: "creatures#index"
 
+  get "/creatures", to: "creatures#index", as: "creatures"
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "/creatures", to: "creatures#create"
+  get "/creatures/:id", to: "creatures#show", as: "creature"
 end
