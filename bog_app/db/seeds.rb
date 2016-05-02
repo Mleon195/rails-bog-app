c949a87b8f17cfe466c5f7fd7df2b326c7e1ae67
Mleon195 <mleon@mail.ccsf.edu>
2016-05-02 15:55:07 -0700

    second run

--- bog_app/db/seeds.rb
+++ bog_app/db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-#
-# db/seeds.rb
-#
-
-Creature.create({name: "Luke", description: "Jedi"})
-Creature.create({name: "Darth Vader", description: "Father of Luke"})
